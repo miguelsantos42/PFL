a996ac4d11dba1b8cb032af315850271f9eef5dd
miguelsantos42 <up202008450@up.pt>
2024-11-30 15:40:01 +0000

    few more alines on 3

--- Prolog/Ficha3.pl
+++ Prolog/Ficha3.pl
@@ -1,3 +1,4 @@
+:- use_module(library(lists)).
 %%1
 %a) a) | ?- [a | [b, c, d] ] = [a, b, c, d]  -> True
 %b) [a | b, c, d ] = [a, b, c, d]  -> False
@@ -79,3 +80,28 @@ del_all(X,[X|T],R):-
 del_all(X,[H|T],[H|R]):-
   X =\= H,
   del_all(X,T,R).
+
+%%d)del_all_list(+ListElems, +List1, ?List2)
+del_all_list(_,[],[]).
+del_all_list(_,List1,List1).
+del_all_list([H|T],List1,R):-
+  del_all(H,List1,R1),
+  del_all_list(T,R1,R).
+
+%%e)del_dups(+List1, ?List2)
+del_dups([],[]).
+del_dups([H|T],[H|L]):-
+  \+member(H,T),
+  del_dups(T,L).
+del_dups([H|T],L):-
+  member(H,T),
+  del_dups(T,L).
+
+%%f)list_perm (+L1, +L2)
+list_perm(Lista1,Lista2):-
+  permutation(Lista1,Lista2).
+
+%%g)replicate(+Amount, +Elem, ?List) 
+replicate(0,_,[]).
+replicate(Amount, Elem, List):-
+  replicate
